b82e8b7d6ab2655643120a818aa02e38c597eebf
egraciani <31991616+egraciani@users.noreply.github.com>
2017-10-05 18:55:27 +0200

    Creating HotelBuilder class

--- src/test/java/es/upm/miw/builder/HotelBuilder.java
+++ src/test/java/es/upm/miw/builder/HotelBuilder.java
@@ -1,5 +1,32 @@
 package es.upm.miw.builder;
 
-public class HotelBuilder {
+import es.upm.miw.ecp1.Category;
+import es.upm.miw.ecp1.Hotel;
+import es.upm.miw.ecp1.Reservation;
 
+public class HotelBuilder {
+    
+    private Hotel hotel;
+    private long id;
+    private String name;
+    
+    public HotelBuilder(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    
+    public HotelBuilder category(Category hotelCategory) {
+        hotel.setCategory(hotelCategory);
+        return this;
+    }
+    
+    public HotelBuilder reservation(Reservation hotelReservation) {
+        hotel.setReservation(hotelReservation);
+        return this;
+    }
+    
+    public Hotel build() {
+        return hotel;
+    }
+    
 }
